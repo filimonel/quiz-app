9635c32c10986d7b340a3aa6884025cbb4acf1f8
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-04-07 11:53:52 +1000

    saved all required  dom elements

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1. Get DOM elements
+const alert = document.querySelector("#alert");
+const quizForm = document.querySelector("#quiz-form");
+const questionitem = document.querySelector(".question-item");
+const answerItem = document.querySelector(".answer-item");
+const answer = document.querySelector(".answer");
+const checkButton = document.querySelector("button");
+
+// 2. When check button is clicked..
+
+// 3. Check which answer has been checked.
+
+// 4. If answer is incorrect, text will change to red.
+
+// 5. If answer is correct, text will change to green.
+
+// 6. If all answers are correct, activate alert.
